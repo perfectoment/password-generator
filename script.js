0ecb2e59a2c0f4e076ff3b24639ccd53d7b23b82
Lee Marvin <ryenem8@gmail.com>
2020-09-18 15:19:25 -0700

    added variables

---
+++ script.js
@@ -0,0 +1,17 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
+var lowerCase = "abcdefghijklmnopqrstuv".split("")
+var numberCase = "1234567890".split("")
+var specialCase = "!@#$%^&*+?".split("")
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
