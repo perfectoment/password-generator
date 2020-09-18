56c8a0ae190926836cd61c159c07ca56e133572e
Lee Marvin <ryenem8@gmail.com>
2020-09-18 16:07:21 -0700

    added number length question

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
-var lowerCase = "abcdefghijklmnopqrstuv".split("")
+var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("")
 var numberCase = "1234567890".split("")
 var specialCase = "!@#$%^&*+?".split("")
+var inputLength = ""
+
 // Write password to the #password input
+ 
+function playersAnswers() {
+  inputLength = prompt("How long would you like your password?")
+  if(inputLength < 8 || inputLength > 128)
+    alert("That is not a valid choice")
+  else()
+}
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
