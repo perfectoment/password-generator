2654bdc4a9177147cc733485fc278a9d72257a24
Lee Marvin <ryenem8@gmail.com>
2020-09-18 18:33:36 -0700

    added the rest of the questions

--- script.js
+++ script.js
@@ -4,15 +4,29 @@ var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
 var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("")
 var numberCase = "1234567890".split("")
 var specialCase = "!@#$%^&*+?".split("")
-var inputLength = ""
+var inputLength = 0;
+var lengthquestions ="0"
 
 // Write password to the #password input
  
-function playersAnswers() {
-  inputLength = prompt("How long would you like your password?")
-  if(inputLength < 8 || inputLength > 128)
-    alert("That is not a valid choice")
-  else()
+  function playersQuestions() {
+    inputLength = prompt ("How long would you like your password?");
+    if(inputLength < 8 || inputLength > 128){
+      alert("That is not a valid choice"); {
+        return
+    
+    }
+    
+  }
+    var lowerCaseAnswer=confirm("Would you like to use lowercase letters?");
+
+    var upperCaseAnswer=confirm ("would you like to use upper case letters?");
+
+    var numberCaseAnswer=confirm("would you like to use numbers?");
+
+    var specialCaseAnswer=confirm("would you like to use special characters?");
+    
+
 }
 
 function writePassword() {
@@ -24,4 +38,6 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", playersQuestions);
+  // playersAnswers()
+console.log(playersQuestions());
