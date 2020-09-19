8f546b282edf889cbac4374442ade82e15b8c9df
Lee Marvin <ryenem8@gmail.com>
2020-09-18 19:21:24 -0700

    consolidated values from answers into new array

--- script.js
+++ script.js
@@ -5,39 +5,55 @@ var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("")
 var numberCase = "1234567890".split("")
 var specialCase = "!@#$%^&*+?".split("")
 var inputLength = 0;
-var lengthquestions ="0"
+var finalPassword =[];
 
 // Write password to the #password input
  
-  function playersQuestions() {
-    inputLength = prompt ("How long would you like your password?");
-    if(inputLength < 8 || inputLength > 128){
-      alert("That is not a valid choice"); {
-        return
-    
-    }
+function playersQuestions() {
+  inputLength = prompt ("How long would you like your password?");
+  if(inputLength < 8 || inputLength > 128) {
+    alert("That is not a valid choice"); 
+       return
     
   }
-    var lowerCaseAnswer=confirm("Would you like to use lowercase letters?");
+  
+  
+  var lowerCaseAnswer=confirm("Would you like to use lowercase letters?");
+     
+  var upperCaseAnswer=confirm ("would you like to use upper case letters?");
+     
+  var numberCaseAnswer=confirm("would you like to use numbers?");
+     
+  var specialCaseAnswer=confirm("would you like to use special characters?");
 
-    var upperCaseAnswer=confirm ("would you like to use upper case letters?");
-
-    var numberCaseAnswer=confirm("would you like to use numbers?");
-
-    var specialCaseAnswer=confirm("would you like to use special characters?");
-    
-
-}
+    if(lowerCaseAnswer){
+      finalPassword.push.apply(finalPassword, lowerCase);
+  }
+    if(numberCaseAnswer){
+      finalPassword.push.apply(finalPassword, numberCase);
+  }
+    if(upperCaseAnswer){
+      finalPassword.push.apply(finalPassword, upperCase);
+    }
+    if(specialCaseAnswer){
+      finalPassword.push.apply(finalPassword, specialCase);
+    }
+      console.log(finalPassword);
+  }
 
 function writePassword() {
+  var trueAnswers = playersQuestions();
+
+  if(trueAnswers) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  }
 }
+
+// function generatePassword()
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", playersQuestions);
-  // playersAnswers()
-console.log(playersQuestions());
+
