43f9e176dde07a1d393ce623a41ae936bc1990e2
Lee Marvin <ryenem8@gmail.com>
2020-09-19 19:12:32 -0700

    added randomizers to prompt answers

--- script.js
+++ script.js
@@ -6,75 +6,66 @@ var numberCase = "1234567890".split("")
 var specialCase = "!@#$%^&*+?".split("")
 var inputLength = 0;
 var finalPassword =[];
+var finalRandom =[];
 
 // Write password to the #password input
  
-function playersQuestions() {
+function generatePassword() {
   inputLength = prompt ("How long would you like your password?");
-  if(inputLength < 8 || inputLength > 128) {
+  inputLength=parseInt(inputLength);
+  if(inputLength < 8 || inputLength > 128 || isNaN(inputLength) ) {
+    inputLength = 0
     alert("That is not a valid choice"); 
-       return;
-    
+       return;  
   }
-  
   
   var lowerCaseAnswer=confirm("Would you like to use lowercase letters?");
     if(lowerCaseAnswer){
-      finalPassword.push.apply(finalPassword, lowerCase);
+      finalPassword=finalPassword.concat(lowerCase);
+      var lowerRand = Math.floor(Math.random() * lowerCase.length);
+        finalRandom += lowerCase[lowerRand];
   }
      
   var upperCaseAnswer=confirm ("would you like to use upper case letters?");
     if(upperCaseAnswer){
-      finalPassword.push.apply(finalPassword, upperCase);
+      finalPassword=finalPassword.concat(upperCase);
+      var upperRand = Math.floor(Math.random() * upperCase.length);
+        finalRandom += upperCase[upperRand];
   }
      
   var numberCaseAnswer=confirm("would you like to use numbers?");
     if(numberCaseAnswer){
-      finalPassword.push.apply(finalPassword, numberCase);
-  }
+      finalPassword=finalPassword.concat(numberCase);
+      var numberRand = Math.floor(Math.random() * numberCase.length);
+        finalRandom += numberCase[numberRand];
+  }   
      
   var specialCaseAnswer=confirm("would you like to use special characters?");
     if(specialCaseAnswer){
-      finalPassword.push.apply(finalPassword, specialCase);  
+      finalPassword=finalPassword.concat(specialCase);
+        var specialRand = Math.floor(Math.random() * specialCase.length);
+          finalRandom += specialCase[specialRand];  
   }
     
 
-
-  //   if(lowerCaseAnswer){
-  //     finalPassword.push.apply(finalPassword, lowerCase);
-  // }
-  //   else if(numberCaseAnswer){
-  //     finalPassword.push.apply(finalPassword, numberCase);
-  // }
-  //   else if(upperCaseAnswer){
-  //     finalPassword.push.apply(finalPassword, upperCase);
-  //   }
-  //   else if(specialCaseAnswer){
-  //     finalPassword.push.apply(finalPassword, specialCase);
-  //   }
-  
-      console.log(finalPassword);
+var passwordOutput = ""
+passwordOutput += passwordOutput.concat(finalRandom)
+for(i=0; i < inputLength-finalRandom.length; i++) {
+  var randomizer = Math.floor(Math.random() * finalPassword.length);
+    passwordOutput += finalPassword[randomizer];
   }
-
+  return passwordOutput;
+}
 function writePassword() {    
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
 }
-function generatePassword(){
-
-  var passwordOutput = ""
-
-  for(i=0; i < inputLength; i++) {
-    var randomizer = Math.floor(Math.random() * finalPassword.length);
-    passwordOutput = passwordOutput + finalPassword[randomizer];
-  }
-  return passwordOutput;
   
 
-}
+
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", playersQuestions);
+// generateBtn.addEventListener("click", playersQuestions);
 generateBtn.addEventListener("click", writePassword);
